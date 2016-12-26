cf507ee51c288bf34981fc1c3d5668cef9372e5a
David Wu <david.wuu@gmail.com>
2016-12-26 01:16:44 -0800

    spacing

--- src/MyBot.js
+++ src/MyBot.js
@@ -9,170 +9,173 @@ const inspect = require('util').inspect;
 
 
 const constants = {
-  baseTileValue: 1,
+	baseTileValue: 1,
 }
 
 
 let turnCount = 0;
 network.on('map', function(gameMap){
-  setSiteStats(gameMap, turnCount++);
-  setSiteFronts(gameMap);
+	setStatsBefore(gameMap, turnCount++);
+	setSiteFronts(gameMap);
+	setStatsAfter(gameMap);
 
-  const moves = [];
-  gameMap.eachMySites(function(site){
-    if(site.isSmall(1)){return}
-    site.getMove(moves);
-  })
-
-  network.sendMoves(moves);
+	const moves = [];
+	gameMap.eachMySites(function(site){
+		if(site.isSmall(1)){return}
+		site.getMove(moves);
+	})
+	network.sendMoves(moves);
 });
 
-function setSiteStats(gameMap, seed){
-  gameMap.eachSite(function(site, x, y){
-      site.number = seed+x+y;
-  })
+function setStatsBefore(gameMap, seed){
+	gameMap.eachSite(function(site, x, y){
+			site.number = seed+x+y;
+	})
 }
 
 function setSiteFronts(gameMap){
-  _.times(8, function(){
-    setXFronts(gameMap);
-    setYFronts(gameMap);
-  })
+	_.times(6, function(){
+		setXFronts(gameMap);
+		setYFronts(gameMap);
+	})
+}
+
+function setStatsAfter(gameMap){
 }
 
 function setFrontState(front={}, site={}, direction){
-    setFrontBaseStats(front, site)
-    setFrontCanCapture(front, site)
-    setFrontDistanceToHostile(front, site)
-    setFrontEfficiency(front, site)
-    _.extend(site.fronts[direction], front)
+		setFrontBaseStats(front, site)
+		setFrontCanCapture(front, site)
+		setFrontDistanceToHostile(front, site)
+		setFrontEfficiency(front, site)
+		_.extend(site.fronts[direction], front)
 
-    setToMostEfficientFront(front, site)
+		setToMostEfficientFront(front, site)
 
-    return front;
+		return front;
 }
 function setFrontBaseStats(front, site){
-    if(site.isMine){
-      front.frontProductionLostOnTheWay += front.production;
-      front.producedOnTheWay += front.productionTo
-      front.productionTo += site.production
-      front.strengthTo += site.strength
-      front.distanceTo += 1
-    }else{
-      front.frontProductionLostOnTheWay = 0;
-      front.producedOnTheWay = 0;
-      front.productionTo = 0
-      front.strengthTo = 0
-      front.distanceTo = 0
+		if(site.isMine){
+			front.frontProductionLostOnTheWay += front.production;
+			front.producedOnTheWay += front.productionTo
+			front.productionTo += site.production
+			front.strengthTo += site.strength
+			front.distanceTo += 1
+		}else{
+			front.frontProductionLostOnTheWay = 0;
+			front.producedOnTheWay = 0;
+			front.productionTo = 0
+			front.strengthTo = 0
+			front.distanceTo = 0
 
-      front.site = site
-      front.strength = site.strength
-      front.production = site.production
-    }
+			front.site = site
+			front.strength = site.strength
+			front.production = site.production
+		}
 
-    front.strengthOnArrival = front.strengthTo + front.producedOnTheWay;
+		front.strengthOnArrival = front.strengthTo + front.producedOnTheWay;
 }
 function setFrontCanCapture(front, site){
-  if(!front.site){
-    return front.canCapture = false;
-  }
-  if(front.site.isHostile){
-    return front.canCapture = true
-  }
+	if(!front.site){
+		return front.canCapture = false;
+	}
+	if(front.site.isHostile){
+		return front.canCapture = true
+	}
 
-  front.canCapture = front.strengthOnArrival > front.strength
-  front.alreadyCanCapture = (front.strengthOnArrival-site.strength) > front.strength
+	front.canCapture = front.strengthOnArrival > front.strength
+	front.alreadyCanCapture = (front.strengthOnArrival-site.strength) > front.strength
 }
 
 function setFrontDistanceToHostile(front, site){
-    if(site.isHostile){
-      front.distanceToHostile = 0
-    }else{
-      front.distanceToHostile++;
-    }
+		if(site.isHostile){
+			front.distanceToHostile = 0
+		}else{
+			front.distanceToHostile++;
+		}
 
-    if(_.isUndefined(site.distanceToHostile) || front.distanceToHostile<site.distanceToHostile){
-      site.distanceToHostile = front.distanceToHostile
-    }else{
-      front.distanceToHostile = site.distanceToHostile
-    }
+		if(_.isUndefined(site.distanceToHostile) || front.distanceToHostile<site.distanceToHostile){
+			site.distanceToHostile = front.distanceToHostile
+		}else{
+			front.distanceToHostile = site.distanceToHostile
+		}
 }
 
 function setFrontEfficiency(front, site){
-  if(!site.isMine){return;}
+	if(!site.isMine){return;}
 
-  const frontValue = front.production + constants.baseTileValue
+	const frontValue = front.production + constants.baseTileValue
 
-  const pressureToStayLocal = 0;
-  const frontCost = front.strength + front.productionTo + front.frontProductionLostOnTheWay + pressureToStayLocal
-  front.efficiency = frontValue/frontCost
+	const pressureToStayLocal = 0;
+	const frontCost = front.strength + front.productionTo + front.frontProductionLostOnTheWay + pressureToStayLocal
+	front.efficiency = frontValue/frontCost
 }
 
 function setToMostEfficientFront(front, site){
-  if(!site.isMine){return;}
-  // if(front.alreadyCanCapture){return;}
-  if(!site.mostEfficientFront || front.efficiency>site.mostEfficientFront.efficiency){
-    site.mostEfficientFront = _.clone(front);
-  }else{
-    _.extend(front, site.mostEfficientFront);
-  }
+	if(!site.isMine){return;}
+	// if(front.alreadyCanCapture){return;}
+	if(!site.mostEfficientFront || front.efficiency>site.mostEfficientFront.efficiency){
+		site.mostEfficientFront = _.clone(front);
+	}else{
+		_.extend(front, site.mostEfficientFront);
+	}
 }
 
 
 const defaultFrontState = {
-  frontProductionLostOnTheWay: 0,
-  producedOnTheWay: 0,
-  productionTo: 0,
-  strengthTo: 0,
-  distanceTo: Infinity,
-  distanceToHostile: Infinity,
-  strengthOnArrival: 0,
-  path: [],
+	frontProductionLostOnTheWay: 0,
+	producedOnTheWay: 0,
+	productionTo: 0,
+	strengthTo: 0,
+	distanceTo: Infinity,
+	distanceToHostile: Infinity,
+	strengthOnArrival: 0,
+	path: [],
 };
 
 // Double loop in all directions
 // First loop sets closest gray square Location (if any)
 // Second lop sets distance to gray square
 function setXFronts(gameMap){
-  let site;
-  const doubleWidth = gameMap.width*2;
+	let site;
+	const doubleWidth = gameMap.width*2;
 
-  let frontState;
-  for(let y=0; y<gameMap.height; y++){
+	let frontState;
+	for(let y=0; y<gameMap.height; y++){
 
-    frontState = _.clone(defaultFrontState);
-    for(let x=0; x<doubleWidth; x++){
-      site = gameMap.getSite({x,y});
-      setFrontState(frontState, site, 'west');
-    }
+		frontState = _.clone(defaultFrontState);
+		for(let x=0; x<doubleWidth; x++){
+			site = gameMap.getSite({x,y});
+			setFrontState(frontState, site, 'west');
+		}
 
-    frontState = _.clone(defaultFrontState);
-    for(let x=doubleWidth-1; x>=0; x--){
-      site = gameMap.getSite({x,y});
-      setFrontState(frontState, site, 'east');
-    }
-  }
+		frontState = _.clone(defaultFrontState);
+		for(let x=doubleWidth-1; x>=0; x--){
+			site = gameMap.getSite({x,y});
+			setFrontState(frontState, site, 'east');
+		}
+	}
 }
 
 function setYFronts(gameMap){
-  let site;
-  const doubleHeight = gameMap.height*2;
+	let site;
+	const doubleHeight = gameMap.height*2;
 
-  let frontState;
-  for(let x=0; x<gameMap.width; x++){
+	let frontState;
+	for(let x=0; x<gameMap.width; x++){
 
-    frontState = _.clone(defaultFrontState);
-    for(let y=0; y<doubleHeight; y++){
-      site = gameMap.getSite({x,y});
-      setFrontState(frontState, site, 'north');
-    }
+		frontState = _.clone(defaultFrontState);
+		for(let y=0; y<doubleHeight; y++){
+			site = gameMap.getSite({x,y});
+			setFrontState(frontState, site, 'north');
+		}
 
-    frontState = _.clone(defaultFrontState);
-    for(let y=doubleHeight-1; y>=0; y--){
-      site = gameMap.getSite({x,y});
-      setFrontState(frontState, site, 'south');
-    }
-  }
+		frontState = _.clone(defaultFrontState);
+		for(let y=doubleHeight-1; y>=0; y--){
+			site = gameMap.getSite({x,y});
+			setFrontState(frontState, site, 'south');
+		}
+	}
 }
 
 
--- src/Site.js
+++ src/Site.js
@@ -99,79 +99,78 @@ class Site {
 	}
 
 
-  getMilitaryMove(moves=[]){
-    let targetSites = this.getNeighborsByHostility();
-    // targetSites.length = 2;
+	getMilitaryMove(moves=[]){
+		let targetSites = this.getNeighborsByHostility();
+		targetSites.length = 2;
 
-    _.each(targetSites, (targetSite)=>{
+		_.each(targetSites, (targetSite)=>{
 
-      if(targetSite.number%2){return;}
-      // if(targetSite.isNeutral && targetSite.strength>=this.strength){return;}
+			if(targetSite.number%2){return;}
+			if(targetSite.isNeutral && targetSite.strength>=this.strength){return;}
 
-		// TODO: remove duplicate block
-		var expectedWaste = this.getExpectedWaste(targetSite)
-		if(expectedWaste - targetSite.strength > 0){return}
-		// Try to continue this move by pushing targetSite
-		if(expectedWaste > 0){
-			if(targetSite.moved){return}
-			// prevent 2 sites from endlessly pushing each other
-			if(targetSite.pushedBy===this){return}
-			targetSite.pushedBy = this
-			this.getMove(moves)
-			// failed to push targetSite, discontinue move
-			if(!targetSite.moved){return}
-		}
-		this.moveTo(targetSite, moves)
-		return false
-		// TODO: remove duplicate block
-    })
-  }
+			// TODO: remove duplicate block
+			var expectedWaste = this.getExpectedWaste(targetSite)
+			if(expectedWaste - targetSite.strength > 0){return}
+			// Try to continue this move by pushing targetSite
+			if(expectedWaste > 0){
+				if(targetSite.moved){return}
+				// prevent 2 sites from endlessly pushing each other
+				if(targetSite.pushedBy===this){return}
+				targetSite.pushedBy = this
+				this.getMove(moves)
+				// failed to push targetSite, discontinue move
+				if(!targetSite.moved){return}
+			}
+			this.moveTo(targetSite, moves)
+			return false
+			// TODO: remove duplicate block
+		})
+	}
 
-  getEconomicMove(moves=[]){
-    let targetFronts = this.getFrontsByEfficiency();
-    targetFronts.length = 2;
+	getEconomicMove(moves=[]){
+		let targetFronts = this.getFrontsByEfficiency();
+		// targetFronts.length = 3;
 
-    // if(!this.pushedBy){
-    //   targetFronts.length=1;
-    // }
+		// if(!this.pushedBy){
+		//   targetFronts.length=1;
+		// }
 
-    _.each(targetFronts, (front)=>{
+		_.each(targetFronts, (front)=>{
+			if(!this.pushedBy || this.strength<200){
+				if(!front.canCapture){
+					return false;
+				}
+			}
+			// consider other options if front is already being captured and site is too small
+			if(front.alreadyCanCapture && this.isSmall(5)){
+				return;
+			}
 
-      if(!this.pushedBy || this.strength<200){
-        if(!front.canCapture){
-          return false;
-        }
-        // consider other options if front is already being captured and site is too small
-        if(front.alreadyCanCapture && this.isSmall(5)){
-          return;
-        }
-      }
+			const targetSite = this.getNeighbor(front.key);
 
-      const targetSite = this.getNeighbor(front.key);
-
-		// TODO: remove duplicate block
-		var expectedWaste = this.getExpectedWaste(targetSite)
-		if(expectedWaste - targetSite.strength > 0){return}
-		// Try to continue this move by pushing targetSite
-		if(expectedWaste > 0){
-			if(targetSite.moved){return}
-			// prevent 2 sites from endlessly pushing each other
-			if(targetSite.pushedBy===this){return}
-			targetSite.pushedBy = this
-			this.getMove(moves)
-			// failed to push targetSite, discontinue move
-			if(!targetSite.moved){return}
-		}
-		this.moveTo(targetSite, moves)
-		return false
-		// TODO: remove duplicate block
-    })
+			// TODO: remove duplicate block
+			var expectedWaste = this.getExpectedWaste(targetSite)
+			if(expectedWaste - targetSite.strength > 0){return}
+			// Try to continue this move by pushing targetSite
+			if(expectedWaste > 0){
+				if(targetSite.moved){return}
+				// prevent 2 sites from endlessly pushing each other
+				if(targetSite.pushedBy===this){return}
+				targetSite.pushedBy = this
+				this.getMove(moves)
+				// failed to push targetSite, discontinue move
+				if(!targetSite.moved){return}
+			}
+			this.moveTo(targetSite, moves)
+			return false
+			// TODO: remove duplicate block
+		})
   }
 
 
 
 	getExpectedWaste(targetSite){
-		return this.strength + targetSite.expectedStrength()-255;
+		return this.strength + targetSite.expectedStrength() - 255;
 	}
 
 	expectedStrength(){
